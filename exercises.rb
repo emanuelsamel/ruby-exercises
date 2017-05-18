51dbd51a413bf98e32494462ebc9eb3d8daea4e9
emanuelsamel <mannys94@hotmail.com>
2017-05-17 20:28:03 -0400

    exercise1

---
+++ exercises.rb
@@ -0,0 +1,14 @@
+
+favcolors = ["green", "lime-green", "mat-blue", "red"]
+ages = [23, 24, 25, 20,]
+cointoss = ["head", "tail", "tail", "head", "tail" ]
+artist = ["drake", "kanye", "future"]
+favcolours = [:green, :limegreen, :matblue, :red]
+
+wdef = {smart: "capable", patience: "calm", endurance: "strong"}
+movies = {iamlegend: 2007, terminator: 1987, transporter: 2000}
+cities = {bangkok: 10045678, montreal: 3000000, lasvegas: 2000000}
+age = {angie: 23, sonya: 24, sonia: 30, ori: 20}
+
+
+puts movies[:iamlegend]
