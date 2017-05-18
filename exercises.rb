37f9f23dcf1166ac7d80b06a850ba7b46b9985e6
emanuelsamel <mannys94@hotmail.com>
2017-05-18 16:04:56 -0400

    exercise6

--- exercises.rb
+++ exercises.rb
@@ -8,7 +8,11 @@ favcolours = [:green, :limegreen, :matblue, :red]
 wdef = {smart: "capable", patience: "calm", endurance: "strong"}
 movies = {iamlegend: 2007, terminator: 1987, transporter: 2000}
 cities = {bangkok: 10045678, montreal: 3000000, lasvegas: 2000000}
-age = {angie: 23, sonya: 24, sonia: 30, ori: 20}
+siblingsandothers = {
+  :angie => {age: 23, :relationship => "sister"},
+  :sonya => {age: 24, :relationship =>"sister"},
+  :sonia => {age: 30, :relationship => "cousin"}
+}
 
 
 # artist.each do |opinion|
@@ -25,5 +29,21 @@ age = {angie: 23, sonya: 24, sonia: 30, ori: 20}
 # puts movies
 # puts cointoss.count("head")
  # puts ages.select { |a| a < 25 }
-artist.delete("drake")
-puts artist
+# artist.delete("drake")
+# puts artist
+# puts "whats your name?"
+# total = 0
+# cities.values.each do |value|
+#   total += value
+# end
+# puts total
+# siblingsandothers.each do |name, hash|
+#   if hash[:age] > 29
+#     puts "#{name} is old "
+#   else
+#     puts "#{name} is young"
+#   end
+# end
+# puts favcolours.last(2)
+# favcolours << ["black", "orange"]
+# puts favcolours
